7849333f77bd1a1cf3b6f25a2f37ce4a792a8198
Peter Ohler <peter@ohler.com>
2017-11-12 19:46:10 -0800

    Docs (#3)
    Moved docs to html and removed from the header files.

--- src/ojc/types.h
+++ src/ojc/types.h
@@ -40,107 +40,44 @@ extern "C" {
 #include <stdint.h>
 #include <stdbool.h>
 
-struct _Buf;
+    struct _Buf;
     
-/**
- * The value types found in JSON documents and in the __ojcVal__ type.
- * @see ojcVal
- */
-typedef enum {
-    /** null */
-    OJC_NULL	= 0,
-    /** string */
-    OJC_STRING	= 's',
-    /** number as a string */
-    OJC_NUMBER	= 'N',
-    /** integer */
-    OJC_FIXNUM	= 'n',
-    /** decimal number */
-    OJC_DECIMAL	= 'd',
-    /** true */
-    OJC_TRUE	= 't',
-    /** false */
-    OJC_FALSE	= 'f',
-    /** array */
-    OJC_ARRAY	= 'a',
-    /** object */
-    OJC_OBJECT	= 'o',
-    /** word with a maximum of 15 characters */
-    OJC_WORD	= 'w',
-    /** opaque which is ignored on write */
-    OJC_OPAQUE	= 'i',
-    /** freed */
-    OJC_FREE	= '#',
-} ojcValType;
+    typedef enum {
+	OJC_NULL	= 0,
+	OJC_STRING	= 's',
+	OJC_NUMBER	= 'N',
+	OJC_FIXNUM	= 'n',
+	OJC_DECIMAL	= 'd',
+	OJC_TRUE	= 't',
+	OJC_FALSE	= 'f',
+	OJC_ARRAY	= 'a',
+	OJC_OBJECT	= 'o',
+	OJC_WORD	= 'w',
+	OJC_OPAQUE	= 'i',
+	OJC_FREE	= '#',
+    } ojcValType;
 
-/**
- * Error codes for the __code__ field in __ojcErr__ structs.
- * @see ojcErr
- */
-typedef enum {
-    /** okay, no error */
-    OJC_OK		= 0,
-    /** type error */
-    OJC_TYPE_ERR	= 't',
-    /** parse error */
-    OJC_PARSE_ERR	= 'p',
-    /** incomplete parse error */
-    OJC_INCOMPLETE_ERR	= 'i',
-    /** buffer overflow error */
-    OJC_OVERFLOW_ERR	= 'o',
-    /** write error */
-    OJC_WRITE_ERR	= 'w',
-    /** memory error */
-    OJC_MEMORY_ERR	= 'm',
-    /** unicode error */
-    OJC_UNICODE_ERR	= 'u',
-    /** abort callback parsing */
-    OJC_ABORT_ERR	= '@',
-    /** argument error */
-    OJC_ARG_ERR		= 'a',
-} ojcErrCode;
+    typedef enum {
+	OJC_OK			= 0,
+	OJC_TYPE_ERR		= 't',
+	OJC_PARSE_ERR		= 'p',
+	OJC_INCOMPLETE_ERR	= 'i',
+	OJC_OVERFLOW_ERR	= 'o',
+	OJC_WRITE_ERR		= 'w',
+	OJC_MEMORY_ERR		= 'm',
+	OJC_UNICODE_ERR		= 'u',
+	OJC_ABORT_ERR		= '@',
+	OJC_ARG_ERR		= 'a',
+    } ojcErrCode;
 
-/**
- * The struct used to report errors or status after a function returns. The
- * struct must be initialized before use as most calls that take an err argument
- * will return immediately if an error has already occurred.
- *
- * @see ojcErrCode
- */
-typedef struct _ojcErr {
-    /** Error code identifying the type of error. */
-    int		code;
-    /** Error message associated with a failure if the code is not __OJC_OK__. */
-    char	msg[256];
-} *ojcErr;
+    typedef struct _ojcErr {
+	int	code;
+	char	msg[256];
+    } *ojcErr;
 
-/**
- * The type representing a value in a JSON file. Values will be one of the
- * __ojcValType__ enum.
- * @see ojcValType
- */
-typedef struct _ojcVal	*ojcVal;
-
-/**
- * When using the parser callbacks, the callback must be of this type. The
- * arguments will be the context provided when calling the parser and the value
- * extracted from the JSON stream.
- *
- * A return value of __true__ indicates the parser should free the values after
- * the callback returns. If the return value is false then the callback is
- * responsible for freeing the value when no longer needed.
- *
- * @see ojc_parse_str, ojc_parse_stream, ojc_parse_socket
- */
-typedef bool	(*ojcParseCallback)(ojcErr err, ojcVal val, void *ctx);
-
-/**
- * Function type to use when providing a custom reader to the parser. Useful for
- * hooking up zlib or similar libraries.
- *
- * @see ojc_parse_reader
- */
-typedef ssize_t	(*ojcReadFunc)(void *src, char *buf, size_t size);
+    typedef struct _ojcVal	*ojcVal;
+    typedef bool		(*ojcParseCallback)(ojcErr err, ojcVal val, void *ctx);
+    typedef ssize_t		(*ojcReadFunc)(void *src, char *buf, size_t size);
 
 #ifdef __cplusplus
 }
